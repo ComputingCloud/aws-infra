25f99864750f343c7b4d03adc1f59401e7f69c5f
parmar.ja@northeastern.edu <jatinparmar06@gmail.com>
2023-03-02 22:30:26 -0500

    terraform final

--- environment/demo/main.tf
+++ environment/demo/main.tf
@@ -19,12 +19,12 @@ module "web_app" {
   # Input Variables
   cidr_name    = var.cidr_name
   vpc_tag_name = var.vpc_tag_name
-  aws_region      = var.aws_region
+  aws_region   = var.aws_region
 
-  aws_subnet_count=var.aws_subnet_count
+  aws_subnet_count = var.aws_subnet_count
 
-  my_ip            = var.my_ip
-  my_ami           = var.my_ami
+  # my_ip             = var.my_ip
+  my_ami            = var.my_ami
   db_storage_size   = var.db_storage_size
   db_instance_class = var.db_instance_class
   db_engine         = var.db_engine
